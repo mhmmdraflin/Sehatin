9725c9eae1d43a3be135cf8614938bf0f75d61c0
mhmmdraflin <muhammadraflinurfathan@gmail.com>
2026-02-03 14:37:57 +0700

    # menambahkan Fungsi Kode pada LoginActivity.kt, RegisterActivity.kt, dan UserDataActivity.kt (fix contains Problems) - belum menambahkan dashboard - melihat data hanya baru bisa dilihat dari log cat # Pembaruan ke 3 - memperbaiki depedencies implemetation(libs.androidx.activity.ktx)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,5 +49,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("androidx.activity:activity-ktx:1.12.3")
+    implementation(libs.androidx.activity.ktx)
 }
