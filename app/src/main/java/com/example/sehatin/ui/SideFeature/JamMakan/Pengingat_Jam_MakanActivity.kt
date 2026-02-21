90c6d31259656fe72b356af40d064d9d9995b228
mhmmdraflin <muhammadraflinurfathan@gmail.com>
2026-02-21 12:40:00 +0800

    # menambahkan Fungsi Kode xml: - Menambahkan Tampilan Pengingat_Jam_MakanActivity.kt - Menambahkan dialog_form_jam_makan.xml - Menambahkan item_jam_makan.xml
    # belum menambahkan:
    - Fitur Informasi Makanan
    - Fitur Olahraga
    
    # Pembaruan (perbaikan & penghapusan fungsi yang tidak digunakan)
    - none

---
+++ app/src/main/java/com/example/sehatin/ui/SideFeature/JamMakan/Pengingat_Jam_MakanActivity.kt
@@ -0,0 +1,21 @@
+package com.example.sehatin.ui.SideFeature.JamMakan
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.sehatin.R
+
+class Pengingat_Jam_MakanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pengingat_jam_makan)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
