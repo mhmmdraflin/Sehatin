d58c6ce4bb4265f318074ae1d2a39b9c19d41974
mhmmdraflin <muhammadraflinurfathan@gmail.com>
2026-02-23 12:00:32 +0800

    # menambahkan Fungsi Kode xml: - Menambahkan Tampilan activity_detail_informasi_makanan.xml
    # belum menambahkan:
    - none
    
    # Pembaruan (perbaikan & penghapusan fungsi yang tidak digunakan)
    - memperbarui DetailInformasiMakananActivity.kt.kt

--- app/src/main/java/com/example/sehatin/ui/SideFeature/DetailInformasiMakananActivity.kt
+++ app/src/main/java/com/example/sehatin/ui/SideFeature/DetailInformasiMakananActivity.kt
@@ -3,8 +3,6 @@ package com.example.sehatin.ui.SideFeature
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.sehatin.R
 
 class DetailInformasiMakananActivity : AppCompatActivity() {
