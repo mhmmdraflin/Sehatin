96b5271859f48a722fbdf2a24eea46c590a3e36b
mhmmdraflin <muhammadraflinurfathan@gmail.com>
2026-03-02 13:43:14 +0800

    # menambahkan Fungsi Kode xml: - menambahkan fragment_dashboard_girl.xml - menambahkan ic_chest - menambahkan detail aktivitas tantangan - menambahkan inventaris - menambahkan preview tantangan - menambahkan dialog jeda tantangan - menambahkan tukar poin
    # belum menambahkan:
    - none
    
    # Pembaruan (perbaikan & penghapusan fungsi yang tidak digunakan)
    - memperbarui user data menambahkan gender laki laki dan perempuan
    - memperbarui kode tukar poin
    - memperbaiki hardcoded dan description image

--- app/src/main/java/com/example/sehatin/ui/Tantangan/CongratulationsActivity.kt
+++ app/src/main/java/com/example/sehatin/ui/Tantangan/CongratulationsActivity.kt
@@ -12,10 +12,5 @@ class CongratulationsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_congratulations)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
--- app/src/main/java/com/example/sehatin/ui/Tantangan/DetailAktivitasActivity.kt
+++ app/src/main/java/com/example/sehatin/ui/Tantangan/DetailAktivitasActivity.kt
@@ -12,10 +12,5 @@ class DetailAktivitasActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_detail_aktivitas)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
