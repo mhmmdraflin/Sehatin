bc7fa7095674122505a93236553ec1bf4bcd5e50
mhmmdraflin <muhammadraflinurfathan@gmail.com>
2026-03-03 16:39:47 +0800

    # menambahkan Fungsi Kode xml: - Menambahkan Tampilan activity_detail_tantangan_makanan.xml - Menambahkan Tampilan activity_detail_aktivitas_makanan.xml - Menambahkan Tampilan activity_congratulations_makanan.xml
    # belum menambahkan:
    - none
    
    # Pembaruan (perbaikan & penghapusan fungsi yang tidak digunakan)
    - None

--- app/src/main/java/com/example/sehatin/ui/Tantangan/CongratulationsMakananActivity.kt
+++ app/src/main/java/com/example/sehatin/ui/Tantangan/CongratulationsMakananActivity.kt
@@ -12,10 +12,5 @@ class CongratulationsMakananActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_congratulations_makanan)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
